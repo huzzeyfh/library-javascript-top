bafd26d4a1e0e5bb078f469164216012d22a22c4
huzzeyfh <hudzaifah7103@gmail.com>
2025-10-16 09:23:01 +0700

    Add an array of books, and constructor
    and function to add books to the array

---
+++ script.js
@@ -0,0 +1,22 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.id = crypto.randomUUID();
+}
+
+function addBookToLibrary(title, author, pages, read) {
+  // take params, create a book then store it in the array
+  const book = new Book(title, author, pages, read);
+  myLibrary.push(book);
+  return book;
+}
+
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, true);
+addBookToLibrary("1984", "George Orwell", 328, false);
+addBookToLibrary("To Kill a Mockingbird", "Harper Lee", 281, true);
+
+console.log(myLibrary);
