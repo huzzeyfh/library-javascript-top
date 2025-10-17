fb24b9e65754b824065b3b462bb445f9eebb3e5d
huzzeyfh <hudzaifah7103@gmail.com>
2025-10-17 21:20:01 +0700

    Add dialog form for adding new book
    Pretty good for first time innit?

--- script.js
+++ script.js
@@ -1,35 +1,59 @@
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.read = read;
   this.id = crypto.randomUUID();
 }
 
-function addBookToLibrary(title, author, pages, read) {
+function addBookToLibrary(title, author, pages) {
   // take params, create a book then store it in the array
-  const book = new Book(title, author, pages, read);
+  const book = new Book(title, author, pages);
   myLibrary.push(book);
   return book;
 }
 
-const booksContainer = document.querySelector('.books');
+const booksContainer = document.querySelector(".books");
 
 function renderLibrary() {
-    booksContainer.innerHTML = '';
-    myLibrary.forEach(book => {
-        const bookDiv = document.createElement('div');
-        bookDiv.classList.add('book');
-        bookDiv.textContent = `${book.title} by ${book.author}, ${book.pages} pages.`
+  booksContainer.innerHTML = "";
+  myLibrary.forEach((book) => {
+    const bookDiv = document.createElement("div");
+    bookDiv.classList.add("book");
+    bookDiv.textContent = `${book.title} by ${book.author}, ${book.pages} pages.`;
 
-        booksContainer.appendChild(bookDiv)
-    })
+    booksContainer.appendChild(bookDiv);
+  });
 }
 
-addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, true);
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295);
 
 renderLibrary();
 
 console.log(myLibrary);
+
+// addBook variables
+const addBook = document.querySelector("#add-book");
+const theDialog = document.querySelector("#add-book-dialog");
+const outputCard = document.querySelector("output");
+const submit = document.querySelector("#submit-btn");
+
+// dialog variables
+const bookTitle = document.querySelector("#book-title");
+const bookAuthor = document.querySelector("#book-author");
+const bookPages = document.querySelector("#book-pages");
+
+addBook.addEventListener("click", () => {
+  theDialog.showModal();
+  submit.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    addBookToLibrary(bookTitle.value, bookAuthor.value, bookPages.value);
+    renderLibrary();
+
+    theDialog.close();
+  });
+});
+
+theDialog.addEventListener("close", () => {});
