db53fb24776e97d23b8b912ac2c82ea29b080327
huzzeyfh <hudzaifah7103@gmail.com>
2025-10-18 16:24:28 +0700

    Add removeBtn to every books added
    and make it works

--- script.js
+++ script.js
@@ -1,37 +1,6 @@
 const myLibrary = [];
 
-function Book(title, author, pages) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.id = crypto.randomUUID();
-}
-
-function addBookToLibrary(title, author, pages) {
-  // take params, create a book then store it in the array
-  const book = new Book(title, author, pages);
-  myLibrary.push(book);
-  return book;
-}
-
 const booksContainer = document.querySelector(".books");
-
-function renderLibrary() {
-  booksContainer.innerHTML = "";
-  myLibrary.forEach((book) => {
-    const bookDiv = document.createElement("div");
-    bookDiv.classList.add("book");
-    bookDiv.textContent = `${book.title} by ${book.author}, ${book.pages} pages.`;
-
-    booksContainer.appendChild(bookDiv);
-  });
-}
-
-addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295);
-
-renderLibrary();
-
-console.log(myLibrary);
 
 // addBook variables
 const addBook = document.querySelector("#add-book");
@@ -40,20 +9,70 @@ const outputCard = document.querySelector("output");
 const submit = document.querySelector("#submit-btn");
 
 // dialog variables
+const form = document.querySelector("form");
 const bookTitle = document.querySelector("#book-title");
 const bookAuthor = document.querySelector("#book-author");
 const bookPages = document.querySelector("#book-pages");
 
+const pages = Number(bookPages.value);
+
+function Book(title, author, pages) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.id = crypto.randomUUID();
+}
+
+//functions
+
+function addBookToLibrary(title, author, pages) {
+  // take params, create a book then store it in the array
+  const book = new Book(title, author, pages);
+  myLibrary.push(book);
+  return book;
+}
+
+function renderLibrary() {
+  booksContainer.innerHTML = "";
+  myLibrary.forEach((book) => {
+    const bookDiv = document.createElement("div");
+    bookDiv.classList.add("book");
+    bookDiv.textContent = `${book.title} by ${book.author}, ${book.pages} pages.`;
+
+    const removeBtn = document.createElement("button");
+    removeBtn.textContent = "Remove";
+    removeBtn.dataset.id = `${book.id}`;
+
+    bookDiv.appendChild(removeBtn);
+
+    booksContainer.appendChild(bookDiv);
+
+    removeBtn.addEventListener("click", (e) => {
+      const objId = e.target.dataset.id
+      const findBook = myLibrary.findIndex(obj => obj.id === objId);
+
+      if (findBook !== -1) {
+        myLibrary.splice(findBook, 1);
+        bookDiv.remove();
+      }
+    });
+
+  });
+}
+
 addBook.addEventListener("click", () => {
   theDialog.showModal();
-  submit.addEventListener("click", (event) => {
-    event.preventDefault();
+});
 
-    addBookToLibrary(bookTitle.value, bookAuthor.value, bookPages.value);
-    renderLibrary();
+submit.addEventListener("click", (event) => {
+  event.preventDefault();
 
-    theDialog.close();
-  });
+  addBookToLibrary(bookTitle.value, bookAuthor.value, bookPages.value);
+  renderLibrary();
+  form.reset();
+  console.log(myLibrary);
+
+  theDialog.close();
 });
 
 theDialog.addEventListener("close", () => {});
